e5b3d2f7edc510131a84d401f52cc2d8620d4b8d
Patrick Passarella <patrick.passarella@toptal.com>
2022-10-28 09:08:31 -0300

    feat: Added queryParams example

--- lib/mocks/handlers.ts
+++ lib/mocks/handlers.ts
@@ -5,7 +5,9 @@ import { dealsMock } from './game/deals'
 export const handlers = [
   rest.get('/api/deals', (req, res, ctx) => {
     const { searchParams } = req.url
-    
+    // Para pehar os query params
+    const price = searchParams.get('price')
+
     return res(ctx.json(dealsMock))
-  }),
+  })
 ]
